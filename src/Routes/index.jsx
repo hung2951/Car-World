b5405ed74a4cefcce4c18137669eb6169b821e68
hung2951 <hungtv2951@gmail.com>
2025-03-04 09:55:13 +0700

    update homepage create footer

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Route,Routes } from 'react-router-dom'
-import Header from '../components/Layouts/header/Header'
 import Layout from '../components/Layouts'
 import HomePage from '../components/Pages/home/HomePage'
 
